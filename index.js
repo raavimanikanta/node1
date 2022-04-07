b9fd4c6fe8dd836b23fdc45c85be5d656e2a82d0
raavimanikanta <raavi,ani123@gmail.com>
2022-04-07 19:19:11 +0530

    second changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 http.createServer(function(request, response){
     response.write("<h1>Hello</h1>");
+    response.write("<h1>Node Js</h1>");
     response.write("<h1>Hello Maniknata</h1>");
     let User={name:"manikanta",age:23,college:"pragati"}
     response.write(JSON.stringify(User));
