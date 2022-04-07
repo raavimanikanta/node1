b57aa53f388748d76fed7badfecf9cd3d6a4ba83
raavimanikanta <raavi,ani123@gmail.com>
2022-04-07 19:13:49 +0530

    first changes

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require("http");
+http.createServer(function(request, response){
+    response.write("<h1>Hello</h1>");
+    response.write("<h1>Hello Maniknata</h1>");
+    let User={name:"manikanta",age:23,college:"pragati"}
+    response.write(JSON.stringify(User));
+    response.end();
+}).listen(3000)
